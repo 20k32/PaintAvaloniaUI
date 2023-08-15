480cc904d670889a673aa92d70b53682ffc565ee
20k32 <egormobtel@gmail.com>
2023-08-15 16:49:55 +0300

    added canvas and base events

--- Paint_AvaloniaUI/ViewLocator/ViewLocator.cs
+++ Paint_AvaloniaUI/ViewLocator/ViewLocator.cs
@@ -12,23 +12,18 @@ namespace Paint_AvaloniaUI.ViewLocator
     {
         public Control? Build(object? param)
         {
-            if(param is not null) 
+            var fileName = param.GetType().FullName;
+
+            var fullFileName = fileName.Replace("ViewModel", "View");
+
+            var type = Type.GetType(fullFileName);
+
+            if (type is not null)
             {
-                var fileName = param.GetType().FullName;
-
-                var fullFileName = fileName.Replace("ViewModel", "View");
-
-                var type = Type.GetType(fullFileName);
-
-                if (type is not null)
-                {
-                    return Activator.CreateInstance(type) as Control;
-                }
-
-                return new TextBlock() { Text = $"File {fullFileName} not found for {fileName}." };
+                return Activator.CreateInstance(type) as Control;
             }
 
-            return new TextBlock() { Text = $"param is null in ViewLocator's build method." };
+            return new TextBlock() { Text = $"File {fullFileName} not found for {fileName}." };
         }
 
         public bool Match(object? data) =>
--- Paint_AvaloniaUI/ViewModels/ControlViewModels/PaintCanvasViewModel.cs
+++ Paint_AvaloniaUI/ViewModels/ControlViewModels/PaintCanvasViewModel.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Paint_AvaloniaUI.ViewModels.ControlViewModels
 {
-    internal class PaintCanvasViewModel : ViewModelBase
+
+    public static class PointerPressedExtensions
     {
+        public static Point GetCoordsRelativeToCanvas(this PointerEventArgs e) =>
+            e.GetPosition((Canvas)e.Source!);
+    }
+
+    internal partial class PaintCanvasViewModel : ViewModelBase
+    {
+        public ObservableCollection<Shape> Shapes { get; private set; } = null!;
+        public Canvas RelativeCanvas { get; set; } = null!;
+
+        public PaintCanvasViewModel()
+        {
+            Shapes = new();
+            RelativeCanvas = new();
+        }
+
+        [RelayCommand]
+        public void CanvasPointerPressed(PointerPressedEventArgs e)
+        {
+            
+        }
+
+        [RelayCommand]
+        public void CanvasPointerReleased(PointerReleasedEventArgs e)
+        {
+            
+        }
+
+        [RelayCommand]
+        public void CanvasPointerMoved(PointerEventArgs e)
+        {
+            Debug.WriteLine(e.GetCoordsRelativeToCanvas());
+        }
     }
 }
