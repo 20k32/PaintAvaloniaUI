22404b7455532ae2cd0a3569579e885ea5acb39b
20k32 <egormobtel@gmail.com>
2023-08-15 15:20:10 +0300

    added main page layout

---
+++ Paint_AvaloniaUI/Views/CotrolViews/PaintCanvas.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Paint_AvaloniaUI.Views.CotrolViews
+{
+    public partial class PaintCanvas : UserControl
+    {
+        public PaintCanvas()
+        {
+            InitializeComponent();
+        }
+    }
+}
